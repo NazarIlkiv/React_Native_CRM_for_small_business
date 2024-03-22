eacbf9d8ceadc2e5ff99fcb7aabd7ed8de99cd0d
nazarilkiv <bruem35@gmail.com>
2024-03-22 10:42:42 +0200

    add localization file

---
+++ localizations/localizations.js
@@ -0,0 +1,12 @@
+export const translations = {
+  en: {
+    SinIn: "Sign In",
+    SinUp: "Sign Up",
+    Language: "Language",
+  },
+  ua: {
+    SinIn: "Увійти",
+    SinUp: "Зареєструватися",
+    Language: "Мова",
+  },
+};
